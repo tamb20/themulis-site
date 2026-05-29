155b8abd8de7e7559f6740c53600c4bad3bff1f1
Dr.Tariq Mohana Bahwini <tamb20@gmail.com>
2026-05-29 13:01:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.lang-switch button').forEach((button) => {
-  button.addEventListener('click', () => {
-    document.querySelectorAll('.lang-switch button').forEach((btn) => btn.classList.remove('active'));
-    button.classList.add('active');
-  });
-});
